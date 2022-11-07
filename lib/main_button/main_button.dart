c0d65b204f1f718b46ea0e23871c6e4127ec92fd
mohamedabdelghany72 <mabdelghany455y@gmail.com>
2022-11-07 23:02:09 +0200

    edit button by abdelghany

--- lib/main_button/main_button.dart
+++ lib/main_button/main_button.dart
@@ -53,78 +53,74 @@ class LoadingButtonState extends State<LoadingButton>
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    return Scaffold(
-      body: Center(
-        child: GestureDetector(
-            onTapDown: (_) => controller.forward(),
-            onTapUp: (_) {
-              if (controller.status == AnimationStatus.forward) {
-                controller.reverse();
-              }
-            },
-            child: Container(
-              decoration: BoxDecoration(
-                  shape: BoxShape.circle, color: myFavColor.withOpacity(0.9),
-              ),
-              child: Stack(
-                alignment: Alignment.center,
-                children: <Widget>[
-                  SizedBox(
-                    height: size.height * 0.3,
-                    width: size.width * 0.65,
-                    child: CircularProgressIndicator(
-                      strokeWidth: 17,
-                      value: 1.0,
-                      valueColor:
-                      AlwaysStoppedAnimation<Color>(Theme.of(context).scaffoldBackgroundColor),
-                    ),
-                  ),
-                  Container(
-                    height: size.height * 0.3,
-                    width: size.width * 0.65,
-                    decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      boxShadow: [
-                        BoxShadow(
-                          color: myFavColor4,
-                          spreadRadius: 6,
-                          blurRadius: 9,
-                          blurStyle: BlurStyle.outer,
-                        ),
-                      ],
-                    ),
-                    child: CircularProgressIndicator(
-                      strokeWidth: 15,
-                      value: 1.0,
-                      valueColor:
-                          AlwaysStoppedAnimation<Color>(Colors.grey[400]!),
-                    ),
-                  ),
-                  SizedBox(
-                    height: size.height * 0.3,
-                    width: size.width * 0.65,
-                    child: CircularProgressIndicator(
-                      strokeWidth: 15,
-                      value: controller.value,
-                      valueColor: AlwaysStoppedAnimation<Color>(myFavColor),
-                    ),
-                  ),
-                  if (controller.status == AnimationStatus.dismissed)
-                    SvgPicture.asset("assets/images/semilogo.svg")
-                  else if (controller.status == AnimationStatus.forward)
-                    Text(
-                      secondsRemaining.toString(),
-                      style: Theme.of(context).textTheme.button!.copyWith(fontSize: 25),
-                    )
-                  else if (controller.status == AnimationStatus.reverse)
-                    SvgPicture.asset("assets/images/semilogo.svg")
-                  else if (controller.status == AnimationStatus.completed)
-                    SvgPicture.asset('assets/images/semilogo.svg')
-                ],
+    return GestureDetector(
+      onTapDown: (_) => controller.forward(),
+      onTapUp: (_) {
+        if (controller.status == AnimationStatus.forward) {
+          controller.reverse();
+        }
+      },
+      child: Container(
+        decoration: BoxDecoration(
+          shape: BoxShape.circle, color: myFavColor.withOpacity(0.9),
+        ),
+        child: Stack(
+          alignment: Alignment.center,
+          children: <Widget>[
+            SizedBox(
+              height: size.height * 0.3,
+              width: size.width * 0.65,
+              child: CircularProgressIndicator(
+                strokeWidth: 17,
+                value: 1.0,
+                valueColor:
+                AlwaysStoppedAnimation<Color>(Theme.of(context).scaffoldBackgroundColor),
               ),
             ),
-          ),
+            Container(
+              height: size.height * 0.3,
+              width: size.width * 0.65,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                boxShadow: [
+                  BoxShadow(
+                    color: myFavColor4,
+                    spreadRadius: 6,
+                    blurRadius: 9,
+                    blurStyle: BlurStyle.outer,
+                  ),
+                ],
+              ),
+              child: CircularProgressIndicator(
+                strokeWidth: 15,
+                value: 1.0,
+                valueColor:
+                AlwaysStoppedAnimation<Color>(Colors.grey[400]!),
+              ),
+            ),
+            SizedBox(
+              height: size.height * 0.3,
+              width: size.width * 0.65,
+              child: CircularProgressIndicator(
+                strokeWidth: 15,
+                value: controller.value,
+                valueColor: AlwaysStoppedAnimation<Color>(myFavColor),
+              ),
+            ),
+            if (controller.status == AnimationStatus.dismissed)
+              SvgPicture.asset("assets/images/semilogo.svg")
+            else if (controller.status == AnimationStatus.forward)
+              Text(
+                secondsRemaining.toString(),
+                style: Theme.of(context).textTheme.button!.copyWith(fontSize: 25),
+              )
+            else if (controller.status == AnimationStatus.reverse)
+                SvgPicture.asset("assets/images/semilogo.svg")
+              else if (controller.status == AnimationStatus.completed)
+                  SvgPicture.asset('assets/images/semilogo.svg')
+          ],
         ),
+      ),
     );
   }
 
