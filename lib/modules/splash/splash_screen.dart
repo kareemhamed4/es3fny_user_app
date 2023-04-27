39d82f27a0f8c96c5bdaec08f470bceca0073798
Kareem Hamed <kareemhamed818@gmail.com>
2023-04-27 02:59:56 +0200

    Fingerprint by kareem

--- lib/modules/splash/splash_screen.dart
+++ lib/modules/splash/splash_screen.dart
@@ -1,8 +1,10 @@
 import 'dart:async';
 import 'dart:math' as math;
+import 'package:es3fny_user_app/biometric/biometric_helper.dart';
 import 'package:es3fny_user_app/layout/layout_screen.dart';
 import 'package:es3fny_user_app/shared/components/components.dart';
 import 'package:es3fny_user_app/shared/constants/constants.dart';
+import 'package:es3fny_user_app/shared/styles/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
 
@@ -13,37 +15,94 @@ class SplashScreen extends StatefulWidget {
   State<SplashScreen> createState() => _SplashScreenState();
 }
 
-class _SplashScreenState extends State<SplashScreen> with TickerProviderStateMixin{
-
+class _SplashScreenState extends State<SplashScreen>
+    with TickerProviderStateMixin {
   late AnimationController controller;
+  bool showBiometric = false;
+  bool isAuthenticated = false;
 
   @override
   void initState() {
+    isBiometricsAvailable();
+    isAuthenticatedDone();
+    listenForAuthentication();
     super.initState();
-    controller = AnimationController(vsync: this, duration: const Duration(seconds: 2))..forward();
-    Timer(const Duration(seconds: 2), () {
-      NavigateToReb(context: context, widget: const LayoutScreen());
-    });
   }
+
+  Future<void> listenForAuthentication() async {
+    bool isAuthenticated = false;
+    while (!isAuthenticated) {
+      try {
+          if (this.isAuthenticated) {
+            controller =
+            AnimationController(vsync: this, duration: const Duration(seconds: 2))
+              ..forward();
+            Timer(const Duration(seconds: 2), () {
+              NavigateToReb(context: context, widget: const LayoutScreen());
+            });
+            isAuthenticated = true;
+            setState(() {});
+          } else {
+            setState(() {});
+          }
+      } catch (e) {
+        setState(() {});
+      }
+      await Future.delayed(const Duration(microseconds: 650));
+    }
+  }
+
+  isBiometricsAvailable() async {
+    showBiometric = await BiometricHelper().hasEnrolledBiometrics();
+    setState(() {});
+  }
+
+  isAuthenticatedDone() async {
+    isAuthenticated = await BiometricHelper().authenticate();
+    setState(() {});
+  }
+
   @override
   dispose() {
     controller.dispose(); // you need this
     super.dispose();
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: SizedBox(
-          child: Transform(
-            alignment: Alignment.center,
-            transform: langCode == "ar" ? Matrix4.rotationY(math.pi) :Matrix4.rotationY(math.pi * 2),
-            child: Lottie.asset(
-              "assets/lottie/ambulance.json",
-              width: 150,
-              height: 150,
+      body: SizedBox(
+        width: double.infinity,
+        child: Stack(
+          alignment: AlignmentDirectional.bottomStart,
+          children: [
+            Center(
+              child: Transform(
+                alignment: Alignment.center,
+                transform: langCode == "ar"
+                    ? Matrix4.rotationY(math.pi)
+                    : Matrix4.rotationY(math.pi * 2),
+                child: Lottie.asset(
+                  "assets/lottie/ambulance.json",
+                  width: 150,
+                  height: 150,
+                ),
+              ),
             ),
-          ),
+            Padding(
+              padding: const EdgeInsets.all(20),
+              child: IconButton(
+                onPressed: () {
+                  isAuthenticatedDone();
+                },
+                icon: Icon(
+                  Icons.fingerprint_outlined,
+                  size: 50,
+                  color: myFavColor5,
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
